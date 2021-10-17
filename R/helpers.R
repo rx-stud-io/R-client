c30366b7401c034481ed473bed2580d232e45141
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2021-10-18 01:02:21 +0200

    check type

--- R/helpers.R
+++ R/helpers.R
@@ -4,12 +4,19 @@
 #' @param tinf optional Infusion Time (number in hours)
 #' @param type Regimen type, e.g. \code{REGIMEN} or \code{ORALREGIMEN}
 #' @return list
+#' @importFrom checkmate assert_number
 #' @export
 #' @examples
 #' regimen(2000)
 #' regimen(2000, 8, 2)
 #' regimen(2000, 12, type = 'ORALREGIMEN')
-regimen <- function(dose, interval = NULL, tinf = NULL, type = 'REGIMEN') {
+regimen <- function(dose, interval = NULL, tinf = NULL, type = c('REGIMEN', 'ORALREGIMEN')) {
+    ## check args
+    assert_number(dose)
+    assert_number(interval, null.ok = TRUE)
+    assert_number(tinf, null.ok = TRUE)
+    type <- match.arg(type)
+    ## return
     Filter(
         Negate(is.null),
         list(DOSE = dose, INTERVAL = interval, TINF = tinf, set = type))
--- inst/tests/testthat/test-helpers.R
+++ inst/tests/testthat/test-helpers.R
@@ -6,5 +6,6 @@ context('generators')
 test_that('regimens', {
     expect_error(regimen())
     expect_error(regimen(TINF = 2))
+    expect_error(regimen('2000'))
     expect_equal(regimen(2000), list(DOSE = 2000, set = 'REGIMEN'))
 })
