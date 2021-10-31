64faf6bca2258399342562a7255e23df39758282
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2021-10-31 22:05:26 +0100

    stop for status

--- R/http.R
+++ R/http.R
@@ -2,9 +2,18 @@
 #' @param endpoint path under \url{https://api.rx.studio/v1/simulation/}
 #' @param ... named parameters passed
 #' @keywords internal
-#' @importFrom httr POST
+#' @importFrom httr POST stop_for_status
 #' @importFrom jsonlite toJSON
+#' @examples \dontrun{
+#' simulate('cefepime_mc_pta_ftime_above_mic',
+#'   PATID = 'Anonymous', AGE = 37, HEIGHT = 175, WEIGHT = 90, GENDER = 'MALE',
+#'   CREATININE = 1, MODEL = 'Tam et al. (2003) - General ward',
+#'   PCTABOVEMIC = 60, CRCLCAP = 'No cap', REGIMENS = list(regimen(1000, 12, 2)))
+#' }
 simulate <- function(endpoint, ...) {
-    POST(url = file.path('https://api.rx.studio/v1/simulation', endpoint),
-         body = toJSON(list(...), auto_unbox = TRUE))
+    res <- POST(
+        url = file.path('https://api.rx.studio/v1/simulation', endpoint),
+        body = toJSON(list(...), auto_unbox = TRUE))
+    stop_for_status(res)
+    content(res)
 }
