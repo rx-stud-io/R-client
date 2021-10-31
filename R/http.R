81417d69866dc03118b2485b6f60af5f88465a52
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2021-10-31 22:29:42 +0100

    return custom class

--- R/http.R
+++ R/http.R
@@ -10,7 +10,7 @@ api_client_id <- '074ba4733a420cc95b50f5036d5beb7a'
 #' @importFrom jsonlite toJSON
 #' @examples \dontrun{
 #' simulate('cefepime-mc-pta-ftime-above-mic',
-#'   PATID = 'Anonymous', AGE = 37, HEIGHT = 175, WEIGHT = 90, GENDER = 'MALE',
+#'   PATID = 'Anonymous', AGE = 37, HEIGHT = 175, WEIGHT = 90, GENDER = 'Male',
 #'   CREATININE = 1, MODEL = 'Tam et al. (2003) - General ward',
 #'   PCTABOVEMIC = 60, CRCLCAP = 'No cap', REGIMENS = list(regimen(1000, 12, 2)))
 #' }
@@ -23,5 +23,6 @@ simulate <- function(endpoint, ...) {
     res <- POST(url = url, body = body, add_headers('X-Api-Key' = api_client_id))
     log_info('Response received in {as.numeric(difftime(Sys.time(), timer, units = "secs"))} seconds.')
     stop_for_status(res)
-    content(res)
+    res <- content(res)
+    structure(res, class = c('rx_studio_report', class(res)))
 }
