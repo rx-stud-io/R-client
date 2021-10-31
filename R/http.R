3fc07f5ae56843a3b7e29680a44787f6efca0dfc
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2021-10-31 22:11:47 +0100

    log start/end

--- R/http.R
+++ R/http.R
@@ -13,11 +13,14 @@ client_id <- '074ba4733a420cc95b50f5036d5beb7a'
 #'   CREATININE = 1, MODEL = 'Tam et al. (2003) - General ward',
 #'   PCTABOVEMIC = 60, CRCLCAP = 'No cap', REGIMENS = list(regimen(1000, 12, 2)))
 #' }
+#' @importFrom logger log_info
 simulate <- function(endpoint, ...) {
-    res <- POST(
-        url = file.path('https://api.rx.studio/v1/simulation', endpoint),
-        body = toJSON(list(...), auto_unbox = TRUE),
-        add_headers('X-Api-Key' = client_id))
+    url <- file.path('https://api.rx.studio/v1/simulation', endpoint)
+    timer <- Sys.time()
+    log_info('Sending request to {url}')
+    body <- toJSON(list(...), auto_unbox = TRUE)
+    res <- POST(url = url, body = body, add_headers('X-Api-Key' = client_id))
+    log_info('Response received in {as.numeric(difftime(Sys.time(), timer, units = "secs"))} seconds.')
     stop_for_status(res)
     content(res)
 }
